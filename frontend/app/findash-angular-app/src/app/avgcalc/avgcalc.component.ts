cef66f83a355e068b4f9b9bedcc33589788864d1
Sergiu <sergiunb@gmail.com>
2023-10-04 16:42:17 +0200

    FIN-10: add empty dashboard with dummy data

---
+++ frontend/app/findash-angular-app/src/app/avgcalc/avgcalc.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-avgcalc',
+  templateUrl: './avgcalc.component.html',
+  styleUrls: ['./avgcalc.component.css']
+})
+export class AvgcalcComponent {
+
+}
