2504f7ec51ffc1ed4f512358efec3bad7d3f9cf0
Sergiu <sergiunb@gmail.com>
2023-10-07 13:28:03 +0200

    FIN-27: backend dashb separate server from db

--- backend/dashboards/src/app.py
+++ backend/dashboards/src/app.py
@@ -1,41 +1,18 @@
 import os 
-import json
 from typing import Optional
 from fastapi import FastAPI, Request, Response, status, APIRouter
-from fastapi.responses import Response, HTMLResponse, PlainTextResponse
-from enum import Enum
+# from fastapi.responses import Response, HTMLResponse, PlainTextResponse
+
 from fastapi.middleware.cors import CORSMiddleware
-from pymongo import MongoClient
 
 from models.dashboard import Dashboard
 # from client import html
 # ------- DB stuff
 # from db import models
 # from db.database import engine
-import time # for some performance tests
-
-MONGODB_SERVER_PORT=27017
-MONGODB_BACKEND = 'mongodb://findemoadmin:findemoadmin_pas_951@datastore-server:27017'
-DATABASE = 'dashboards'
+import libs.dashboards as dashb
 
 FRONTEND_HOST = os.environ.get('FRONTEND_HOST', None)
-
-
-def _get_db_handle():
-    """ 
-    Connect to the Database and return a handle for operations
-
-    Returns
-    -------
-    - db - database handle
-    - client - mongo client handle for connection level operations (i.e. close)
-    """
-
-    client = MongoClient(host=MONGODB_BACKEND, port=27017)
-    db = client[DATABASE]
-
-    return db, client
-
 
 app = FastAPI()
 # set up CORS for same-machine access
@@ -47,10 +24,14 @@ app.add_middleware(
     allow_headers = ['*']               # all headers
 )
 
-
 # -----  Routers - none for this app, we can keep all apis local to module
 
 # ------ APIs
+@app.get("/api/v1/dashboards/test")
+def test_point():
+
+    return "Test-OK"
+
 @app.get("/api/v1/dashboards/load", 
         tags=['dashboards'], 
         summary='retrieve a dashboard configuration',
@@ -69,7 +50,7 @@ async def find_dashboard_for_user_and_name(user: str, dashboard_name:str):
     --------
     - 
     """
-    dashboard = _find_dashboard_by_owner_and_name(user, dashboard_name)
+    dashboard = dashb.find_dashboard_by_owner_and_name(user, dashboard_name)
     
     return dashboard
 
@@ -92,7 +73,7 @@ async def find_last_updated_dashboard_for_user(user: str):
     --------
     - 
     """
-    last_updated_dashboard = _find_last_updated_dashboard_by_user(user)
+    last_updated_dashboard = dashb.find_last_updated_dashboard_by_user(user)
     
     return {'dashboard': last_updated_dashboard}
 
@@ -114,7 +95,7 @@ async def find_all_dashboards_for_user(user: str):
     --------
     - 
     """
-    last_updated_dashboard = _find_all_dashboards_by_user(user)
+    last_updated_dashboard =dashb.find_all_dashboards_by_user(user)
     
     return last_updated_dashboard
 
@@ -125,7 +106,6 @@ async def find_all_dashboards_for_user(user: str):
         description=f'Stores a Dashboard profile given',
         )
 async def save_dashboard(user: str , dashboard:Dashboard):
-# async def save_dashboard(request:Request):
     """
     Retrieves a Dashboard profile given a user/owner and a dashboard name
 
@@ -139,56 +119,7 @@ async def save_dashboard(user: str , dashboard:Dashboard):
     - 
     """
 
-    saved  = _save_dashboard(user, dashboard) 
+    saved  = dashb.save_dashboard(user, dashboard) 
     return {saved is not None}
     
 
-def _find_dashboard_by_owner_and_name(user:str, dashboard_name:str):
-    # ########### DB OPERATIONS 
-    db,client =_get_db_handle()
-    print(user, dashboard_name)
-
-    dashboard = db['dashboard'].find_one({"ownerid":user, "name":dashboard_name}, projection={"_id":0})
-
-    client.close()
-
-    return {'dashboard':dashboard}
-
-def _find_last_updated_dashboard_by_user(user):
-    # ########### DB OPERATIONS 
-    db, client =_get_db_handle()
-
-    dashboard = db["dashboard"].aggregate([
-           {"$match": {"ownerid":user}}, 
-           {"$sort": {"unixTimestamp": -1}},
-           {"$limit": 1 },
-           {"$unset":["_id", "ownerid"]}
-        ]).next()
-
-    client.close()
-    return dashboard
-
-def _find_all_dashboards_by_user(user):
-    # ########### DB OPERATIONS 
-    db, client =_get_db_handle()
-
-    # simply convert to list here, no big data expected
-    dashboards = list(db["dashboard"].find({"ownerid":user}, projection={"_id":0}))
-
-    client.close()
-    return dashboards
-
-
-def _save_dashboard(user:str, dashboard_data: Dashboard):
-
-    # ########### DB OPERATIONS 
-    db,client =_get_db_handle()
-
-    res = db['dashboard'].insert_one({
-                                      "ownerid":user, 
-                                      "name":dashboard_data.name, 
-                                      "trackedSymbols":dashboard_data.trackedSymbols,
-                                      "unixTimestamp": dashboard_data.unixTimestamp
-                                      })
-    client.close()
-    return res.inserted_id    
---
+++ backend/dashboards/src/libs/dashboards.py
@@ -0,0 +1,55 @@
+import json
+from pymongo import MongoClient
+from db.db import get_db_handle
+from models.dashboard import Dashboard
+
+def find_dashboard_by_owner_and_name(user:str, dashboard_name:str):
+    # ########### DB OPERATIONS 
+    db,client = get_db_handle()
+    print(user, dashboard_name)
+
+    dashboard = db['dashboard'].find_one({"ownerid":user, "name":dashboard_name}, projection={"_id":0})
+
+    client.close()
+
+    return {'dashboard':dashboard}
+
+def find_last_updated_dashboard_by_user(user):
+    # ########### DB OPERATIONS 
+    db, client =get_db_handle()
+
+    dashboard = db["dashboard"].aggregate([
+           {"$match": {"ownerid":user}}, 
+           {"$sort": {"unixTimestamp": -1}},
+           {"$limit": 1 },
+           {"$unset":["_id", "ownerid"]}
+        ]).next()
+
+    client.close()
+    return dashboard
+
+def find_all_dashboards_by_user(user):
+    # ########### DB OPERATIONS 
+    db, client =get_db_handle()
+
+    # simply convert to list here, no big data expected
+    dashboards = list(db["dashboard"].find({"ownerid":user}, projection={"_id":0}))
+
+    client.close()
+
+    return dashboards
+
+
+def save_dashboard(user:str, dashboard_data: Dashboard):
+
+    # ########### DB OPERATIONS 
+    db,client =get_db_handle()
+
+    res = db['dashboard'].insert_one({
+                                      "ownerid":user, 
+                                      "name":dashboard_data.name, 
+                                      "trackedSymbols":dashboard_data.trackedSymbols,
+                                      "unixTimestamp": dashboard_data.unixTimestamp
+                                      })
+    client.close()
+    return res.inserted_id    
