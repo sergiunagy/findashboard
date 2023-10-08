a8daf1666cdd18fc83af171bfab373be355c78e4
Sergiu <sergiunb@gmail.com>
2023-10-08 21:56:48 +0200

    FIN-21: Add Frontend edit function

--- frontend/app/findash-angular-app/src/app/model/dashboardconfig.ts
+++ frontend/app/findash-angular-app/src/app/model/dashboardconfig.ts
@@ -6,6 +6,7 @@ import * as moment from "moment";
 
 export interface DashboardConfig {
     id: string;
+    ownerid: string;
     name: string;
     trackedSymbols: string[];
     unixTimestamp: number;
